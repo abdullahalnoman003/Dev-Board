ad3ea62e4a93b88ab35c2a240be7479ad3cec961
abdullahalnoman003 <noman22622@gmail.com>
2025-03-01 10:21:12 +0600

    Added Card Action

---
+++ js/cardAction.js
@@ -0,0 +1,96 @@
+document.getElementById("card-btn1").addEventListener("click", function(){
+    this.disabled = true;
+    let task= document.getElementById("task-assigned").innerText;
+    let finalTask= document.getElementById("total-counter").innerText;
+    let convertedFinalTask = parseInt(finalTask);
+    let convertedTask = parseInt(task);
+    let updatedTask = convertedTask-1;
+    let updatedFinalTask = convertedFinalTask+1;
+    document.getElementById("task-assigned").innerText=updatedTask;
+    document.getElementById("total-counter").innerText=updatedFinalTask;
+    alert("Board Updated Successfully!");
+    if(convertedTask<=1){
+        alert("Task Finished");
+    }
+})
+
+document.getElementById("card-btn2").addEventListener("click", function(){
+    this.disabled = true;
+    let task= document.getElementById("task-assigned").innerText;
+    let finalTask= document.getElementById("total-counter").innerText;
+    let convertedFinalTask = parseInt(finalTask);
+    let convertedTask = parseInt(task);
+    let updatedTask = convertedTask-1;
+    let updatedFinalTask = convertedFinalTask+1;
+    document.getElementById("task-assigned").innerText=updatedTask;
+    document.getElementById("total-counter").innerText=updatedFinalTask;
+    alert("Board Updated Successfully!");
+    if(convertedTask<=1){
+        alert("Task Finished");
+    }
+})
+
+document.getElementById("card-btn3").addEventListener("click", function(){
+    this.disabled = true;
+    let task= document.getElementById("task-assigned").innerText;
+    let finalTask= document.getElementById("total-counter").innerText;
+    let convertedFinalTask = parseInt(finalTask);
+    let convertedTask = parseInt(task);
+    let updatedTask = convertedTask-1;
+    let updatedFinalTask = convertedFinalTask+1;
+    document.getElementById("task-assigned").innerText=updatedTask;
+    document.getElementById("total-counter").innerText=updatedFinalTask;
+    alert("Board Updated Successfully!");
+    if(convertedTask<=1){
+        alert("Task Finished");
+    }
+})
+
+document.getElementById("card-btn4").addEventListener("click", function(){
+    this.disabled = true;
+    let task= document.getElementById("task-assigned").innerText;
+    let finalTask= document.getElementById("total-counter").innerText;
+    let convertedFinalTask = parseInt(finalTask);
+    let convertedTask = parseInt(task);
+    let updatedTask = convertedTask-1;
+    let updatedFinalTask = convertedFinalTask+1;
+    document.getElementById("task-assigned").innerText=updatedTask;
+    document.getElementById("total-counter").innerText=updatedFinalTask;
+    alert("Board Updated Successfully!");
+    if(convertedTask<=1){
+        alert("Task Finished");
+    }
+})
+
+document.getElementById("card-btn5").addEventListener("click", function(){
+    this.disabled = true;
+    let task= document.getElementById("task-assigned").innerText;
+    let finalTask= document.getElementById("total-counter").innerText;
+    let convertedFinalTask = parseInt(finalTask);
+    let convertedTask = parseInt(task);
+    let updatedTask = convertedTask-1;
+    let updatedFinalTask = convertedFinalTask+1;
+    document.getElementById("task-assigned").innerText=updatedTask;
+    document.getElementById("total-counter").innerText=updatedFinalTask;
+    alert("Board Updated Successfully!");
+    if(convertedTask<=1){
+        alert("Task Finished");
+    }
+})
+
+document.getElementById("card-btn6").addEventListener("click", function(){
+    this.disabled = true;
+    let task= document.getElementById("task-assigned").innerText;
+    let finalTask= document.getElementById("total-counter").innerText;
+    let convertedFinalTask = parseInt(finalTask);
+    let convertedTask = parseInt(task);
+    let updatedTask = convertedTask-1;
+    let updatedFinalTask = convertedFinalTask+1;
+    document.getElementById("task-assigned").innerText=updatedTask;
+    document.getElementById("total-counter").innerText=updatedFinalTask;
+    alert("Board Updated Successfully!");
+    if(convertedTask<=1){
+        alert("Task Finished");
+    }
+})
+
